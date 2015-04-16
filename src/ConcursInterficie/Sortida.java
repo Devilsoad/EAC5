c7d16f19eac547d4d411ddeebacc506d64f0872f
Devilsoad <devilsoad22@hotmail.com>
2015-04-16 18:34:51 +0200

    1.1

---
+++ src/ConcursInterficie/Sortida.java
@@ -0,0 +1,15 @@
+package ConcursInterficie;
+import ConcursDades.DadesConcursants;
+/**
+ *
+ * @author Jose
+ */
+public class Sortida {
+    
+    public void mostrarDadesInscripcio(){
+        System.out.println("¿Son correctes aquestes dades?");
+        DadesConcursants.mostrarDades.concursant;
+        
+    }
+    
+}
