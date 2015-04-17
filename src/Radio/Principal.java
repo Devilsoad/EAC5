8662cc53b7d790722fa0345d9d73d0f046453eee
Devilsoad <devilsoad22@hotmail.com>
2015-04-18 00:06:59 +0200

    versió 1.3

--- src/Radio/Principal.java
+++ src/Radio/Principal.java
@@ -1,11 +1,44 @@
 package Radio;
 
+import static Radio.varis.MAXIM_CONCURSANTS;
 import java.util.Scanner;
 /**
  *
  * @author Jose, Llorenç, Silvia
  */
 public class Principal {
+    /**
+     * Nombre maxim de concursants
+     */
+    static int MAXIM_CONCURSANTS = 15;
+    /**
+     * Nombre de camps que emmagatzemarem per concursant
+     */
+    static int CAMPS_CONCURSANTS = 4;
+    /**
+     * Identificador del camp DNI (posicio dins la matriu)
+     */
+    static int DADES_DNI = 0;
+    /**
+     * Identificador del camp COGNOMS (posicio dins la matriu)
+     */
+    static int DADES_COGNOMS = 1;
+    /**
+     * Identificador del camp NOM (posicio dins la matriu)
+     */
+    static int DADES_NOM = 2;
+    /**
+     * Identificador del camp TELEFON (posicio dins la matriu)
+     */
+    static int DADES_TELF = 3;
+    /**
+     * Vector de les lletres d'un dni ordenades d'acord amb l'index usat en el calcul 
+     * de la lletra del dni
+     */
+    public static final String DNICARACTERS = "TRWAGMYFPDXBNJZSQVHLCKE";
+    public static final int MIDATELEFONIDNI = 9;
+    public int comptador = 0;
+    String nom, cognom, DNI, telefon, puntuacio, capcalera, mostrarDades, continuar;;
     public static void main(String[] args) {
         
         Principal programa = new Principal();
@@ -142,5 +175,29 @@ public class Principal {
                 
             break;
         }    
-    }   
+    }
+    //Llista al Twitter dels inscrits.
+    public void llistaInicialConcursants(){
+        System.out.println("===================================\n"
+                + "Publicacio de la llista de participants pel Twitter\n"
+                + "===================================");
+        formatLlistaTwitter();
+        
+    }
+    //Treim les dades per formatetjar-les.
+    public void formatLlistaTwitter(){
+        String [][] dadesParticipants = new String [MAXIM_CONCURSANTS][4];
+        String [] arrayMostrar = new String [MAXIM_CONCURSANTS];
+        dadesParticipants = arrayCaptura();
+        capcalera = String.format("%-11s %-15s %-22s %-11s","DNI","NOM","COGNOM","TELEFON"); 
+        System.out.println(capcalera);
+        for (int i = 0;i < dadesParticipants.length;i++){
+            int j = 0;
+            DNI = dadesParticipants[i][j];
+            nom = dadesParticipants[i][j+1];
+            cognom = dadesParticipants[i][j+2];
+            telefon = dadesParticipants[i][j+3];
+            arrayMostrar[i]= String.format("%-11s %-15s %-22s %-11s",DNI,nom,cognom,telefon);
+        } 
+    }
 }
